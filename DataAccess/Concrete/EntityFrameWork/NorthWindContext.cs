acdecca1e8968265ff5207e4dfd1cbf301b4f0ca
Ebrar HASLAK <Ebrar.HASLAK@gsb.gov.tr>
2022-11-16 17:12:04 +0300

    .

--- DataAccess/Concrete/EntityFrameWork/NorthWindContext.cs
+++ DataAccess/Concrete/EntityFrameWork/NorthWindContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFrameWork
 {
-    public class NorthWindContext
+    public class NorthWindContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer();
+        }
     }
 }
