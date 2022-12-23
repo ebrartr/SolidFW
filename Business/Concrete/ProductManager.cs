78a6edbfacc01756e954b1e9683f0363918c66a1
Ebrar HASLAK <Ebrar.HASLAK@gsb.gov.tr>
2022-12-23 12:18:20 +0300

    code reafactor

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -38,7 +38,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            //if (DateTime.Now.Hour == 16)
+            //if (DateTime.Now.Hour == 11)
             //{
             //    return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime); ;
             //}
